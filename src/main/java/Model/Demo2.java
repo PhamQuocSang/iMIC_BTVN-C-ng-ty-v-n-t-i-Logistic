74f51a21c5e3fc45245d150d05ce066dad57aece
Phạm Quốc Sang <56308260+PhamQuocSang@users.noreply.github.com>
2019-10-13 19:44:00 +0700

    The v1.1 version

---
+++ src/main/java/Model/Demo2.java
@@ -0,0 +1,15 @@
+package Model;
+
+public class Demo2 {
+
+
+    public static void main(String[] args) {
+        Demo2();
+    }
+    public static void Demo2(){
+        String name, classS;
+        //sua len verson moi
+        name = "d";
+        System.out.println("Ten là: " + name);
+    }
+}
